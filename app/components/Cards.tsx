f3d2f0832b88d2d493db870b701324cc5c49b2eb
Henrique Heron <henriqueheron1@gmail.com>
2024-04-24 23:24:04 -0300

    add cards

---
+++ app/components/Cards.tsx
@@ -0,0 +1,64 @@
+import Single from '@/public/single.png';
+import Double from '@/public/double.png';
+import Triple from '@/public/triple.png';
+
+interface Plan {
+  image: string;
+  price: number;
+  storage: string;
+  allowedUsers: number;
+  sendUpTo: string;
+}
+
+export default function Cards(): JSX.Element {
+  const singleUser: Plan = {
+    image: Single.src,
+    price: 149,
+    storage: '500 GB',
+    allowedUsers: 1,
+    sendUpTo: '2 GB',
+  };
+
+  const partnership: Plan = {
+    image: Double.src,
+    price: 199,
+    storage: '1 TB',
+    allowedUsers: 3,
+    sendUpTo: '10 GB',
+  };
+
+  const groupAccount: Plan = {
+    image: Triple.src,
+    price: 299,
+    storage: '5 TB',
+    allowedUsers: 10,
+    sendUpTo: '20 GB',
+  };
+
+  const allPlans: Plan[] = [singleUser, partnership, groupAccount];
+
+  return (
+    <div id="cards" className="text-black bg-white py-16 w-full">
+      <div id="cards-wrapper" className='mx-auto flex items-center justify-center gap-3 border border-red-500'>
+        {allPlans.map((plan) => {
+          return <CardUnit plan={plan} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+function CardUnit({ plan }: { plan: Plan }): JSX.Element {
+  return (
+    <div className="card flex flex-col items-center justify-center shadow-sm shadow-gray-500 hover:bg-gray-200 transition-colors px-5">
+      <img src={plan.image} alt="Single User" className='h-[50px] -top-1' />
+      <h3>${plan.price}</h3>
+      <div className="card-details flex flex-col">
+        <p className='border-t border-t-black'>{plan.storage} storage</p>
+        <p className='border-t border-t-black'>{plan.allowedUsers} {plan.allowedUsers > 1 ? 'Users' : 'User'} Allowed</p>
+        <p className='border-y border-y-black'>Send up to {plan.sendUpTo}</p>
+      </div>
+      <button>Start Trial</button>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
 import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <Analytics />
       <Newsletter />
+      <Cards />
     </>
   );
 }
