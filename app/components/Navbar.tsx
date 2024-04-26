a538758429520bc8dfb974871ac0efda9d50864d
Henrique Heron <henriqueheron1@gmail.com>
2024-04-26 11:00:17 -0300

    Change company name to Lake

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar(): JSX.Element {
       id="navbar"
       className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white"
     >
-      <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">React.</h1>
+      <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">Lake.</h1>
       <ul className="hidden md:flex cursor-pointer">
         <li className="p-4 hover:bg-slate-500 duration-300">Home</li>
         <li className="p-4 hover:bg-slate-500 duration-300">Company</li>
@@ -33,7 +33,7 @@ export default function Navbar(): JSX.Element {
             : 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
         }
       >
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">React.</h1>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">Lake.</h1>
         <ul className="p-4 uppercase">
           <li className="p-4 border-b border-gray-600">Home</li>
           <li className="p-4 border-b border-gray-600">Company</li>
