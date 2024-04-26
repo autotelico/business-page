3e19eb731abd08271e06133b7d76680e46010598
Henrique Heron <henriqueheron1@gmail.com>
2024-04-26 10:37:42 -0300

    add green icons to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,15 +1,17 @@
-import LinkedInIcon from '@/public/linkedin-icon.svg'
+import LinkedInIcon from '@/public/linkedin-green-icon.svg'
+import DiscordIcon from '@/public/discord-green-icon.svg'
+import Email from '@/public/email-green-icon.svg'
 
 export default function Footer(): JSX.Element {
   return (
-    <footer className="py-16 px-8bbg-black text-[#00df9a] flex flex-col items-center justify-center">
-      <div className="">
-        <h4 className="text-3xl">Developed by: Henrique Heron</h4>
-      </div>
-      <div id="social-media" className="grid grid-cols-3 gap-3 mt-5">
-        <img src={LinkedInIcon.src} alt="" className="icon bg-blue-500 w-full" />
-        <img src="" alt="" className="icon bg-blue-500 p-5" />
-        <img src="" alt="" className="icon bg-blue-500 p-5" />
+    <footer className="py-16 px-8bbg-black text-[#00df9a] flex items-center justify-center">
+      <div id="footer-wrapper" className='mx-auto'>
+        <h4 className="text-3xl mx-auto text-center">Developed by: Henrique Heron</h4>
+        <div id="social-media" className="mx-auto grid grid-cols-3 justify-items-center content-center mt-5">
+          <img src={LinkedInIcon.src} alt="Henrique Heron's LinkedIn Profile" className="h-[50px] w-[50px] hover:scale-110 duration-300" />
+          <img src={DiscordIcon.src} alt="Henrique Heron's Discord Profile" className="h-[50px] w-[50px] hover:scale-110 duration-300" />
+          <img src={Email.src} alt="Henrique Heron's Email" className="h-[50px] w-[50px] hover:scale-110 duration-300" />
+        </div>
       </div>
     </footer>
   );
